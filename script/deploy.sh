ade5f79f4b9e331338134a84de8d35f355edc845
Brady Holt <brady.holt@gmail.com>
2013-09-25 16:06:51 -0500

    adding config

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,11 +5,11 @@ git pull
 echo "rake assets:precompile"
 bundle exec rake assets:precompile
 echo "copying files..."
-rsync -rvuz ~/dev/elf/ bholt@geekytidbits.com:web/elf --exclude='.git/' --exclude='log/'
+rsync -rvuz ~/dev/elf/ bholt@gtb:web/elf --exclude='.git/' --exclude='log/'
 echo "bundle install"
-ssh bholt@geekytidbits.com 'cd ~/web/elf && bundle install'
+ssh bholt@gtb 'cd ~/web/elf && bundle install'
 echo "rake tmp:clear"
-ssh bholt@geekytidbits.com 'cd ~/web/elf && bundle exec rake tmp:clear'
+ssh bholt@gtb 'cd ~/web/elf && bundle exec rake tmp:clear'
 echo "touch tmp/restart.txt"
-ssh bholt@geekytidbits.com 'touch ~/web/elf/tmp/restart.txt'
+ssh bholt@gtb 'touch ~/web/elf/tmp/restart.txt'
 echo "Deploy Successful!"
